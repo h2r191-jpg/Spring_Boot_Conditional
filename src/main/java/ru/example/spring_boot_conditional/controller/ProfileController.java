0a3c67526a6dea2d645756cf1dea2a80ebe0027e
h2r191-jpg <h2r191@gmail.com>
2026-03-29 22:02:13 +0700

    Update ProfileController.java
    1

--- src/main/java/ru/example/spring_boot_conditional/controller/ProfileController.java
+++ src/main/java/ru/example/spring_boot_conditional/controller/ProfileController.java
@@ -1,4 +1,21 @@
 package ru.example.spring_boot_conditional.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.example.spring_boot_conditional.interfaces.SystemProfile;
+
+@RestController
+@RequestMapping("/")
 public class ProfileController {
+    private final SystemProfile profile;
+
+    public ProfileController(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("/")
+    public String getProfile() {
+        return profile.getProfile();
+    }
 }
