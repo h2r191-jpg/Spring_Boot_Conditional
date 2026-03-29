603246b52c626f93f36329a7b59f807ef8df4175
h2r191-jpg <h2r191@gmail.com>
2026-03-29 22:01:27 +0700

    Update JavaConfig.java
    1

--- src/main/java/ru/example/spring_boot_conditional/config/JavaConfig.java
+++ src/main/java/ru/example/spring_boot_conditional/config/JavaConfig.java
@@ -1,4 +1,24 @@
 package ru.example.spring_boot_conditional.Config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.example.spring_boot_conditional.profile.DevProfile;
+import ru.example.spring_boot_conditional.profile.ProductionProfile;
+import ru.example.spring_boot_conditional.interfaces.SystemProfile;
+
+@Configuration
 public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+
+        return new DevProfile();
+    }
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false", matchIfMissing = true)
+    public SystemProfile prodProfile() {
+
+        return new ProductionProfile();
+    }
 }
